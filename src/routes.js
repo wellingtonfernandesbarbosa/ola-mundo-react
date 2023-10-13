d9d8860e5e9c321147083fa0d9da528dee00d8d2
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-10-12 23:18:10 -0300

    Implementação da Página Padrão para Estrutura Consistente
    Neste commit, foi implementada uma página padrão para garantir a consistência estrutural do projeto e evitar a repetição de código. Agora, as rotas da página inicial e da página 'Sobre Mim' utilizam a mesma estrutura de página padrão, melhorando a organização do projeto.

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" Component={Inicio} />
-        <Route path="/sobremim" Component={SobreMim} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
         <Route
           path="*"
           element={
